fb85f1c79fea3e8930e5a14ed889b0b5eae6f156
amelonelie <amelie.meyer@bayern-mail.de>
2024-05-30 15:02:26 +0200

    updates

--- population_styling.py
+++ population_styling.py
@@ -1,13 +1,15 @@
 from pyqgis_scripting_ext.core import *
 
+folder = "/Users/amelonelie/Documents/Programme/GitHub/advanced_geomatics/data/"
+output_folder = "/Users/amelonelie/Documents/Programme/GitHub/advanced_geomatics/outputs/"
+geopackagePath = folder + "small_natural_earth.gpkg"
+
 
 HMap.remove_layers_by_name(["Population"])
 
 citiesName = "ne_50m_populated_places"
 countriesName = "ne_50m_admin_0_countries"
 
-folder = "/Users/amelonelie/Documents/Master EMMA/semester 2/advanced geomatics+EIA/advanced geomatics/06/natural_earth_vector.gpkg/packages/"
-geopackagePath = folder + "natural_earth_vector.gpkg"
 
 countriesLayer = HVectorLayer.open(geopackagePath, countriesName)
 countriesFeatures = countriesLayer.features()
@@ -48,7 +50,7 @@ population_layer.set_graduated_style('population', ranges, styles)
 HMap.add_layer(population_layer)    
 
     
-path = folder + "population.gpkg"
-error = population_layer.dump_to_gpkg(path, overwrite=True)
+output_path = output_folder + "population.gpkg"
+error = population_layer.dump_to_gpkg(output_path, overwrite=True)
 if(error):
     print(error)   
