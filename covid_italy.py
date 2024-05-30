2395b70667e51c1afc5004bc8c01dd4dd6e78eee
amelonelie <amelie.meyer@bayern-mail.de>
2024-05-30 13:23:10 +0200

    Update covid_italy.py

--- covid_italy.py
+++ covid_italy.py
@@ -5,31 +5,42 @@ path = f"{folder}dpc-covid19-ita-regioni.csv"
 
 folder2 = "/Users/amelonelie/Documents/Master EMMA/semester 2/advanced geomatics+EIA/advanced geomatics/06/natural_earth_vector.gpkg/packages/"
 geopackagePath = folder2 + "natural_earth_vector.gpkg"
-countriesName = "ne_50m_admin_0_countries"
-citiesName = "ne_50m_populated_places"
-countriesLayer = HVectorLayer.open(geopackagePath, countriesName)
+
+regionsName = "ne_10m_admin_1_states_provinces"
+regionsLayer = HVectorLayer.open(geopackagePath, regionsName)
 
 
-countriesFeatures = countriesLayer.features()
-nameIndex = countriesLayer.field_index("NAME")
-fieldNames = countriesLayer.field_names
-
-
-for feature in countriesFeatures:
-    if feature.attributes[nameIndex] == 'Italy':
-        geom = feature.geometry 
+regionsFeatures = regionsLayer.features()
+nameIndex = regionsLayer.field_index("NAME")
+fieldNames = regionsLayer.field_names
 
 
 with open(path, 'r') as file:
     covid_lines =file.readlines()
     
 
+regions_dict = {
+    "id": "Integer"
+}
 
-
-for line in covid_lines[:5]:
+points = []
+for line in covid_lines[1:5]:
     line = line.strip()   
     line = line.split(",")
-    x_coord = line[4]
-    y_coord = line[5]
+    x_coord = float(line[4])
+    y_coord = float(line[5])
+    point = HPoint(x_coord, y_coord)
+
+region_layer = HVectorLayer.new("Regions", "Polygon", "EPSG:4326", regions_dict)    
   
-    
+for feature in regionsFeatures:
+    geom = feature.geometry 
+    if geom.contains(point):
+        result = geom
+        
+region_layer.add_feature(result,[1])  
+HMap.add_layer(region_layer)
+
+
+# dict key name of the region in csv file, value compele region (geometry)
+#together
