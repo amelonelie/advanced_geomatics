e8f72e965042069d5126381a9d0504d7aeb59739
amelonelie <amelie.meyer@bayern-mail.de>
2024-05-30 15:24:35 +0200

    Update UTM_grid.py

--- UTM_grid.py
+++ UTM_grid.py
@@ -2,20 +2,6 @@
 
 # 60 zones a 6°
 
-# coordinates = []
-# number = 0
-# for i in range(60):
-#     coord = [[number,0], [number,180], [number+6,180], [number+6,0], [number,0]]
-#     coordinates.append(coord)
-#     number +=6
-
-# multipolygon = HMultiPolygon.fromCoords(coordinates)
-# canvas = HMapCanvas.new()
-# canvas.add_geometry(multipolygon, "red", 2)
-# canvas.set_extent([-10, -10, 370, 10])
-# canvas.show()
-
-
 polygons = []
 extent = 6
 for lon in range(-180, 180, extent):
